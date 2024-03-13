adcedd76f65e5844859d82a1bb5457bc05d30526
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-03-13 14:52:59 +0000

    load and show alpaca

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 // Initialize the Image Classifier method with MobileNet
 let mobileNet;
 
+//the image of the alpaca
+let alpaca;
+
 /**
  * Callback for image classifier - logs model is loaded
  */
@@ -8,10 +11,18 @@ const modelLoaded = () => {
   console.log("model is loaded");
 };
 
+const imageLoaded = () => {
+  image(alpaca, 0, 0, width, height);
+}
+
 function setup() {
   //create canvas
   createCanvas(650, 600);
   background(0);
+
+  //load alpaca image
+  alpaca = createImg('images/alpaca.jpeg', imageLoaded);
+  alpaca.hide();
 
   //load model
   mobileNet = ml5.imageClassifier("MobileNet", modelLoaded);
