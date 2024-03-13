c44dcc19c401b87fb172effc8dbd2f9ea22ece5d
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-03-13 14:42:58 +0000

    load ml5 classifier

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+// Initialize the Image Classifier method with MobileNet
+const classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+const modelLoaded = () => {
+  console.log("model is loaded");
+};
+
 function setup() {
-    createCanvas(650, 600);
-    background(0);
+  createCanvas(650, 600);
+  background(0);
 }
-  
-function draw() {
-}
+
+function draw() {}
