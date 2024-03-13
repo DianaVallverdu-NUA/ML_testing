15d9915ad09ac4004a2da0ddf07c95d49849d9cf
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-03-13 14:58:26 +0000

    log results of prediction for one image

--- sketch.js
+++ sketch.js
@@ -5,15 +5,43 @@ let mobileNet;
 let alpaca;
 
 /**
+ * 
+ * @param {*} error
+ * @param {*} results 
+ */
+const gotResults = (error, results) => {
+  //check no errors
+  if(error) {
+    console.error(error);
+    return;
+  }
+
+  //log results
+  console.log(results);
+}
+
+/**
  * Callback for image classifier - logs model is loaded
  */
 const modelLoaded = () => {
   console.log("model is loaded");
+  mobileNet.predict(alpaca, gotResults);
 };
 
+/**
+ * Show image in the canvas
+ */
 const imageLoaded = () => {
-  image(alpaca, 0, 0, width, height);
-}
+  //get width and height
+  let actualWidth = width;
+  let actualHeight = height;
+  if (alpaca.width < alpaca.height)
+    actualWidth = (alpaca.width * height) / alpaca.height;
+  else actualHeight = (alpaca.height * width) / alpaca.width;
+
+  //display
+  image(alpaca, 0, 0, actualWidth, actualHeight);
+};
 
 function setup() {
   //create canvas
@@ -21,7 +49,7 @@ function setup() {
   background(0);
 
   //load alpaca image
-  alpaca = createImg('images/alpaca.jpeg', imageLoaded);
+  alpaca = createImg("images/alpaca.jpeg", imageLoaded);
   alpaca.hide();
 
   //load model
