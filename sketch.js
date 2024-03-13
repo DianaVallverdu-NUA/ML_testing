d8e842a2d0abd2d3b6112efd21ea53146fb3bf38
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-03-13 14:43:55 +0000

    comment

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 // Initialize the Image Classifier method with MobileNet
 const classifier = ml5.imageClassifier("MobileNet", modelLoaded);
 
+/**
+ * Callback for image classifier - logs model is loaded
+ */
 const modelLoaded = () => {
   console.log("model is loaded");
 };
